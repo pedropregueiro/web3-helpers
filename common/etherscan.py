493293b7fddc6f060d3054d0787a3ec30ae9d3cd
prego <pedropregueiro@gmail.com>
2021-12-06 18:05:25 +0100

    ethernet... ugh

--- common/etherscan.py
+++ common/etherscan.py
@@ -2,14 +2,14 @@ import os
 
 import requests
 
-ETHERNET_API_KEY = os.environ.get("ETHERNET_API_KEY")
+ETHERSCAN_API_KEY = os.environ.get("ETHERSCAN_API_KEY")
 POLYGON_API_KEY = os.environ.get("POLYGON_API_KEY")
 
 ETHERSCAN_BASE_URL = 'https://api.etherscan.io/api'
 POLYGON_BASE_URL = 'https://api.polygonscan.com/api'
 
 CHAINS = {
-    "eth": {"url": ETHERSCAN_BASE_URL, "api_key": ETHERNET_API_KEY},
+    "eth": {"url": ETHERSCAN_BASE_URL, "api_key": ETHERSCAN_API_KEY},
     "polygon": {"url": POLYGON_BASE_URL, "api_key": POLYGON_API_KEY}
 }
 
